043f0b986e803cb37f684db3f2b00f3def92f718
Chris <ckeenan@starry.com>
2020-09-10 14:14:36 -0400

    Added buildSrc module with LibraryConstants object

---
+++ buildSrc/src/main/java/com.chrynan.inject.buildSrc/LibraryConstants.kt
@@ -0,0 +1,14 @@
+package com.chrynan.inject.buildSrc
+
+object LibraryConstants {
+
+    const val bintrayRepo = "chrynan"
+    const val group = "com.chrynan.inject"
+    const val owner = "chrynan"
+    const val repoName = "inject"
+    const val versionName = "0.6.0"
+    const val versionCode = 4
+    const val versionDescription = "Minor Release 0.6.0"
+    const val license = "Apache-2.0"
+    const val vcsUrl = "https://github.com/chRyNaN/inject.git"
+}
