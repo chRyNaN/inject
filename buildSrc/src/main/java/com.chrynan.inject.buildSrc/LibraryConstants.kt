9198a22c972b3f1be6b41cc867b53491e5ecac93
Christopher Keenan <ckeenan@starry.com>
2022-02-12 20:43:43 -0600

    Bumped version to 1.0.1 (9)

--- buildSrc/src/main/java/com.chrynan.inject.buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com.chrynan.inject.buildSrc/LibraryConstants.kt
@@ -2,13 +2,12 @@ package com.chrynan.inject.buildSrc
 
 object LibraryConstants {
 
-    const val bintrayRepo = "chrynan"
     const val group = "com.chrynan.inject"
     const val owner = "chrynan"
     const val repoName = "inject"
-    const val versionName = "1.0.0"
-    const val versionCode = 8
-    const val versionDescription = "Release 1.0.0"
+    const val versionName = "1.0.1"
+    const val versionCode = 9
+    const val versionDescription = "Release $versionName ($versionCode)"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/inject.git"
 
